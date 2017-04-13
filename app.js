ed4a15fd6e6c9dc620a1df84b6facf3ce53f6f69
e174596549 <ly6819@hotmail.com>
2017-04-13 12:16:17 +0800

    增加手机适配

--- app.js
+++ app.js
@@ -28,17 +28,20 @@ var sub = function(c) {
     });
 }
 sub();
-
-io = require('socket.io').listen(server); //引入socket.io模块并绑定到服务器
-//socket部分
+// 引入 socket.io 模块并绑定到服务器
+io = require('socket.io').listen(server);
+// 服务器当前在线用户列表
 var nameList = []
+// socket 部分
 io.on('connection', function(socket) {
     console.log('a user connected');
+    // socket 断开时发送下线消息
     socket.on('disconnect', function() {
         console.log('user disconnected');
         nameList.splice(nameList.indexOf(socket.name), 1);
         socket.broadcast.emit('system', socket.name, nameList, 'logout');
     });
+    // 新用户登录时 记录名字并广播给其他用户
     socket.on('name', function(data) {
         var userName = data || '匿名'
         console.log(data);
@@ -48,6 +51,7 @@ io.on('connection', function(socket) {
         socket.emit('loginSuccess', nameList);
         io.sockets.emit('system', userName, nameList, 'login');
     });
+    // 聊天消息
     socket.on('message', function(data) {
         if (data !== '') {
             console.log('message data:', data);
@@ -56,6 +60,7 @@ io.on('connection', function(socket) {
             io.sockets.emit('message', time, socket.name, data)
         }
     });
+    // redis 订阅 系统广播
     redisclient.on('message', function(error, msg) {
         console.log('connection');
         console.log(msg);
